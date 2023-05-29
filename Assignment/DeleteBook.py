561842c939822d3c8c88720b7f91d3b226d1533a
phamduclong2103 <132881046+phamduclong2103@users.noreply.github.com>
2023-05-29 11:40:45 +0700

    Delete Assignment directory

--- Assignment/DeleteBook.py
+++
@@ -1,4 +0,0 @@
-def delete_book(books_list):
-    bid = input("Enter the book ID to delete: ")
-    books_list.delete(bid)
-    print("Book deleted successfully.")
--- Assignment/ViewBooks.py
+++
@@ -1,6 +0,0 @@
-def view_books(books_list):
-    if books_list.is_empty():
-        print("No books available in the library.")
-    else:
-        print("Books in the library:")
-        books_list.display()
